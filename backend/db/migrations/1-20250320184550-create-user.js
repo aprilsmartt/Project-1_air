72c3daabf516faf589a625ae2e43067a20b0a090
aprilsmartt <20917096+aprilsmartt@users.noreply.github.com>
2025-04-23 05:27:18 -0500

    migrate all files old and new for render

--- backend/db/migrations/1-20250320184550-create-user.js
+++ backend/db/migrations/1-20250320184550-create-user.js
@@ -5,7 +5,7 @@ let options = {};
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;  //! define your schema in options object
 }
-// options.tableName = "Users"; //! options.tablename can go here or inside module.exports object
+// options.tableName = "Users"; //! options.tablename can go here or inside module.exports object 
 
 
 module.exports = {

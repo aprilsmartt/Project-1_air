11a23a474ca59ccd94bf152fff01605c46e07316
aprilsmartt <20917096+aprilsmartt@users.noreply.github.com>
2025-05-09 21:08:19 -0500

    update ProfileButton to use dropdown menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,68 +2,75 @@ import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton/OpenModalButton';
+import LoginFormModal from '../LoginFormModal/LoginFormModal';
+import SignupFormModal from '../SignupFormModal/SignupFormModal';
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
-    const ulRef = useRef();
-  
-    const toggleMenu = (e) => {
-      e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-      // if (!showMenu) setShowMenu(true);
-      setShowMenu(!showMenu);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
-  
-    useEffect(() => {
-      if (!showMenu) return;
-  
-      const closeMenu = (e) => {
-        if (ulRef.current && !ulRef.current.contains(e.target)) {
-          setShowMenu(false);
-        }
-      };
-  
-      document.addEventListener('click', closeMenu);
-  
-      return () => document.removeEventListener('click', closeMenu);
-    }, [showMenu]);
-  
-    const logout = (e) => {
-      e.preventDefault();
-      dispatch(sessionActions.logout());
-    };
-  
-    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-  
-    return (
-      <>
-        <button onClick={toggleMenu}>
-          <FaUserCircle />
-        </button>
-        <ul className={ulClassName} ref={ulRef}>
-          <li>{user.username}</li>
-          <li>{user.firstName} {user.lastName}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      </>
-    );
-  }
-  
-  export default ProfileButton;
-          
 
-// ====================================================================
-// To use and change the color of the react icon use the following code
-// import { FaCarrot } from 'react-icons/fa6';
+    document.addEventListener('click', closeMenu);
 
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-// const Carrot = () => {
-//     return (
-//       <div style={{ color: "orange", fontSize: "100px" }}>
-//         <FaCarrot />
-//       </div>
-//     );
-//   };
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  return (
+    <>
+      <button onClick={toggleMenu}>
+        <FaUserCircle />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default ProfileButton;
